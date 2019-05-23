e9bbce3341b0d15ecc323731c72f6a7cbd620f98
Sam Markham <sam.e.markham@gmail.com>
2019-05-23 22:41:39 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,10 +5,7 @@ def my_select(collection)
    new_collection = []
    i = 0 
    while i < collection.length 
-    if collection[i] == true 
-      new_collection << yield(collection[i])
-    else 
-    end 
+    new_collection << yield(collection[i])
     i += 1
     #do stuff that will return the elements being passed through a block if they evaluate true to the block. 
    end 
