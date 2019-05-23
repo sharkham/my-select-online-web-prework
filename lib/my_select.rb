4b3dbadb37ce7ea7e15d604b6ad7dce290933c34
Sam Markham <sam.e.markham@gmail.com>
2019-05-23 22:34:42 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,25 @@
 def my_select(collection)
- # your code here!
+ if collection == [] 
+   "This block should not run!"
+ else 
+   new_collection = []
+   i = 0 
+   while i < collection.length 
+    new_collection << yield(collection[i])
+    i += 1
+    #do stuff that will return the elements being passed through a block if they evaluate true to the block. 
+   end 
+   new_collection
+ end 
 end
+
+  #def select(&block)
+    #result =[]
+    #self.each do |element|
+      #result << element if block.call(element) == true
+    #end
+    #result
+  #end
+  
+      #new_collection2 = yield(collection[i])
+    #new_collection << new_collection2 if (new_collection[i] == true)
